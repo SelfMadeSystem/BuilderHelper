952938e0b04c78c073b955e73adb868d12682c04
sms <creepercannyon@gmail.com>
2021-03-25 08:35:31 -0700

    Initial commit

---
+++ src/main/java/uwu/smsgamer/builderhelper/BuilderHelper.java
@@ -0,0 +1,10 @@
+package uwu.smsgamer.builderhelper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BuilderHelper implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/uwu/smsgamer/builderhelper/client/BuilderHelperClient.java
@@ -0,0 +1,13 @@
+package uwu.smsgamer.builderhelper.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BuilderHelperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
